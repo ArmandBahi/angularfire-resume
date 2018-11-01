97080da89a6831d0c955764cefa15327f9a47696
Armand Bahi <armand.bahi@veremes.com>
2018-11-01 18:22:56 +0100

    Use scss instead sass

--- src/app/pages/notes/notes.component.ts
+++ src/app/pages/notes/notes.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
-  styleUrls: ['./notes.component.sass']
+  styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent implements OnInit {
 
