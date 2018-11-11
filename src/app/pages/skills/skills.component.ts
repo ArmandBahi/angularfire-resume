5bb1088f12b149fe5fbcf7702f6075c1a2de78d1
Armand Bahi <armand.bahi@veremes.com>
2018-11-11 10:42:28 +0100

    Indent

--- src/app/pages/skills/skills.component.ts
+++ src/app/pages/skills/skills.component.ts
@@ -5,96 +5,95 @@ import { map } from 'rxjs/operators';
 import { Materialize } from 'materialize-css';
 
 export interface Skill {
-  name: string;
-  rate: number;
-  id?: string;
+    name: string;
+    rate: number;
+    id?: string;
 }
 
 @Component({
-  selector: 'app-skills',
-  templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+    selector: 'app-skills',
+    templateUrl: './skills.component.html',
+    styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
 
-  skillsCollection: AngularFirestoreCollection<Skill>;
-  skillsDoc: AngularFirestoreDocument<Skill>;
-  skills: Observable<Skill[]>;
-  newSkill: object;
-  showEditBtn: array;
-  showEditForm: array;
-  showNewBtn: boolean;
-  showNewForm: boolean;
+    skillsCollection: AngularFirestoreCollection<Skill>;
+    skillsDoc: AngularFirestoreDocument<Skill>;
+    skills: Observable<Skill[]>;
+    newSkill: object;
+    showEditBtn: array;
+    showEditForm: array;
+    showNewBtn: boolean;
+    showNewForm: boolean;
+    
+    constructor(private afs: AngularFirestore) { }
 
-  constructor(private afs: AngularFirestore) { }
+    /**
+    * ngOnInit - component init
+    *
+    */
+    ngOnInit() {
+        this.skillsCollection = this.afs.collection<Skill>('skills');
+        this.skills = this.skillsCollection.snapshotChanges().pipe(
+            map(actions => actions.map(a => {
+                const data = a.payload.doc.data() as Skill;
+                const id = a.payload.doc.id;
+                return { id, ...data };
+            }))
+        );
+        this.newSkill = {};
+        this.showEditBtn = [];
+        this.showEditForm = [];
+        this.showNewBtn = false;
+        this.showNewForm = false;
+    }
 
+    /**
+    * addSkill - Add a new skill
+    *
+    * @param  {object} newSkill
+    */
+    addSkill(newSkill) {
+        this.skillsCollection.add(newSkill).then(function() {
+            Materialize.toast('Skill successfully added', 3000, 'green');
+            newSkill.name = "";
+            newSkill.rate = "";
+        })
+        .catch(function(error) {
+            Materialize.toast('Error when adding skill', 3000, 'red');
+            console.error("Error writing document: ", error);
+        });
+    }
 
-  /**
-   * ngOnInit - component init
-   *
-   */
-  ngOnInit() {
-    this.skillsCollection = this.afs.collection<Skill>('skills');
-    this.skills = this.skillsCollection.snapshotChanges().pipe(
-      map(actions => actions.map(a => {
-        const data = a.payload.doc.data() as Skill;
-        const id = a.payload.doc.id;
-        return { id, ...data };
-      }))
-    );
-    this.newSkill = {};
-    this.showEditBtn = [];
-    this.showEditForm = [];
-    this.showNewBtn = false;
-    this.showNewForm = false;
-  }
+    /**
+    * updateSkill - Update an existing skill
+    *
+    * @param  {object} oSkill
+    */
+    updateSkill(oSkill) {
+        let skillDoc = this.afs.doc('skills/' + oSkill.id);
+        skillDoc.update(oSkill).then(function() {
+            Materialize.toast('Skill successfully updated', 3000, 'green');
+        })
+        .catch(function(error) {
+            Materialize.toast('Error when updating skill', 3000, 'red');
+            console.error("Error writing document: ", error);
+        });
+    }
 
-  /**
-   * addSkill - Add a new skill
-   *
-   * @param  {object} newSkill
-   */
-  addSkill(newSkill) {
-    this.skillsCollection.add(newSkill).then(function() {
-        Materialize.toast('Skill successfully added', 3000, 'green');
-        newSkill.name = "";
-        newSkill.rate = "";
-    })
-    .catch(function(error) {
-        Materialize.toast('Error when adding skill', 3000, 'red');
-        console.error("Error writing document: ", error);
-    });
-  }
-
-  /**
-   * updateSkill - Update an existing skill
-   *
-   * @param  {object} oSkill
-   */
-  updateSkill(oSkill) {
-    let skillDoc = this.afs.doc('skills/' + oSkill.id);
-    skillDoc.update(oSkill).then(function() {
-        Materialize.toast('Skill successfully updated', 3000, 'green');
-    })
-    .catch(function(error) {
-        Materialize.toast('Error when updating skill', 3000, 'red');
-        console.error("Error writing document: ", error);
-    });
-  }
-
-  /**
-   * deleteSkill - Update an existing skill
-   *
-   * @param  {object} oSkill
-   */
-  deleteSkill(oSkill) {
-    let skillDoc = this.afs.doc('skills/' + oSkill.id);
-    skillDoc.delete().then(function() {
-        Materialize.toast('Skill successfully deleted', 3000, 'green');
-    })
-    .catch(function(error) {
-        Materialize.toast('Error when deleting skill', 3000, 'red');
-        console.error("Error writing document: ", error);
-    });
-  }
+    /**
+    * deleteSkill - Update an existing skill
+    *
+    * @param  {object} oSkill
+    */
+    deleteSkill(oSkill) {
+        let skillDoc = this.afs.doc('skills/' + oSkill.id);
+        skillDoc.delete().then(function() {
+            Materialize.toast('Skill successfully deleted', 3000, 'green');
+        })
+        .catch(function(error) {
+            Materialize.toast('Error when deleting skill', 3000, 'red');
+            console.error("Error writing document: ", error);
+        });
+    }
 }
